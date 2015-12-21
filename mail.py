09712499a90985916fc2442a9ccbb468de2f05e3
Tom Johnson <public@tomjohnson.me.uk>
2015-12-21 22:42:25 +0000

    Added base arguments.

--- mail.py
+++ mail.py
@@ -9,6 +9,9 @@ import jinja2
 
 def main(args):
     parser = argparse.ArgumentParser(description="Builds a CV using a YAML database.")
+    parser.add_argument("source", help="The YAML database to source from.")
+    parser.add_argument("template", help="The template to build the CV from.")
+    parser.add_argument("output", help="The file to output.")
 
     args = parser.parse_args(args)
 
