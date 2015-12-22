4460231b1abf053e8e2bb8581007ccb0e61cae50
Tom Johnson <public@tomjohnson.me.uk>
2015-12-22 17:46:40 +0000

    Tag filtering infrastructure up (currently stubbed).

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ import os.path
 import jinja2
 import yaml
 
+def filter_db(db, tags):
+    return db
+
 def main(args):
     parser = argparse.ArgumentParser(description="Builds a CV using a YAML database.")
     parser.add_argument("source", help="The YAML database to source from.")
     parser.add_argument("template", help="The template to build the CV from.")
     parser.add_argument("output", help="The file to output.")
+    parser.add_argument("--tags", help="A comma-separated list of tags. If present, only items tagged with those specified appear in the output.")
 
     args = parser.parse_args(args)
+
+    tags = None
+    if args.tags is not None:
+        tags = args.tags.split(",")
 
     db = None
     with open(args.source, "r") as source_file:
         db = yaml.load(source_file)
+
+    if tags is not None:
+        db = filter_db(db, tags)
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(args.template)))
     template = None
